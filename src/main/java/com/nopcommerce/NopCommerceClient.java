6d899b13eef2981c3988be40b0dfd2e7fb0d5f80
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2017-01-30 15:54:15 +0200

    Change the sinceId parameter to Integer.

--- src/main/java/com/nopcommerce/NopCommerceClient.java
+++ src/main/java/com/nopcommerce/NopCommerceClient.java
@@ -11,19 +11,19 @@ public interface NopCommerceClient
     int MAXIMUM_API_CALL_LIMIT = 250;
 
     @RequestLine("GET /api/customers?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
-    CustomerList getCustomers(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") String sinceId, @Param("fields") String fields);
+    CustomerList getCustomers(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") Integer sinceId, @Param("fields") String fields);
 
     @RequestLine("GET /api/categories?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
-    CategoryList getCategories(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") String sinceId, @Param("fields") String fields);
+    CategoryList getCategories(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") Integer sinceId, @Param("fields") String fields);
 
     @RequestLine("GET /api/products?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
-    ProductList getProducts(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") String sinceId, @Param("fields") String fields);
+    ProductList getProducts(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") Integer sinceId, @Param("fields") String fields);
 
     @RequestLine("GET /api/product_category_mappings?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
-    ProductCategoryMappingList getProductCategoryMappings(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") String sinceId, @Param("fields") String fields);
+    ProductCategoryMappingList getProductCategoryMappings(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") Integer sinceId, @Param("fields") String fields);
 
     @RequestLine("GET /api/orders?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
-    OrderList getOrders(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") String sinceId, @Param("fields") String fields);
+    OrderList getOrders(@Param("limit") Integer limit, @Param("page") Integer page, @Param("since-id") Integer sinceId, @Param("fields") String fields);
 
     @RequestLine("POST /api/webhooks/registrations")
     WebHook createWebhook(WebHook webhook);
