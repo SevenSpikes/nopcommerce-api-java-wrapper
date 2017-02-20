80efb30ac1369a276d946fd7b4ad5a67364dc27e
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2017-02-20 18:02:27 +0200

    Set the user agent header.

--- src/main/java/com/nopcommerce/auth/ContentTypeRequestInterceptor.java
+++ src/main/java/com/nopcommerce/auth/ContentTypeRequestInterceptor.java
@@ -9,5 +9,6 @@ public class ContentTypeRequestInterceptor implements RequestInterceptor
     public void apply(RequestTemplate template)
     {
         template.header("Content-Type", "application/json");
+        template.header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0");
     }
 }
