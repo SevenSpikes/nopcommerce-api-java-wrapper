3ce3434e881f9b88d67194c62a9a20ef334c0b87
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2017-12-05 12:18:02 +0200

    Set the connection and request timeouts to 2 minutes.

--- src/main/java/com/nopcommerce/NopCommerceFactory.java
+++ src/main/java/com/nopcommerce/NopCommerceFactory.java
@@ -3,6 +3,7 @@ package com.nopcommerce;
 import com.nopcommerce.auth.ContentTypeRequestInterceptor;
 import com.nopcommerce.auth.OAuthRequestInterceptor;
 import feign.Feign;
+import feign.Request;
 import feign.RequestInterceptor;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
@@ -19,12 +20,17 @@ public class NopCommerceFactory
         requestInterceptors.add(new OAuthRequestInterceptor(accessToken));
         requestInterceptors.add(new ContentTypeRequestInterceptor());
 
+        // Set the connection and read timeouts to 2 minutes. In some nopCommerce websites
+        // with a lot of data a request may take 1+ minute.
+        Request.Options feignOptions = new Request.Options(120000, 120000);
+
         NopCommerceClient nopCommerceClient = Feign.builder()
                 .decoder(new JacksonDecoder())
                 .encoder(new JacksonEncoder())
 //                .logger(new Logger.JavaLogger().appendToFile("http.log"))
 //                .logLevel(Logger.Level.FULL)
                 .requestInterceptors(requestInterceptors)
+                .options(feignOptions)
                 .target(NopCommerceClient.class, storeUrl);
 
         return nopCommerceClient;
