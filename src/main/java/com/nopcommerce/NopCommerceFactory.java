330010e1e9c03f00174169d793693bd667afd697
Ivan Stoyanov <ivanst.stoyanov@gmail.com>
2018-04-04 15:35:06 +0300

    Disable the request retrier.

--- src/main/java/com/nopcommerce/NopCommerceFactory.java
+++ src/main/java/com/nopcommerce/NopCommerceFactory.java
@@ -4,6 +4,7 @@ import com.nopcommerce.auth.ContentTypeRequestInterceptor;
 import com.nopcommerce.auth.OAuthRequestInterceptor;
 import feign.Feign;
 import feign.RequestInterceptor;
+import feign.Retryer;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
@@ -19,12 +20,18 @@ public class NopCommerceFactory
         requestInterceptors.add(new OAuthRequestInterceptor(accessToken));
         requestInterceptors.add(new ContentTypeRequestInterceptor());
 
+//        Request.Options feignOptions = new Request.Options(10000, 30000);
+
+        Retryer retryer = new Retryer.Default(0,0,0);
+
         NopCommerceClient nopCommerceClient = Feign.builder()
                 .decoder(new JacksonDecoder())
                 .encoder(new JacksonEncoder())
 //                .logger(new Logger.JavaLogger().appendToFile("http.log"))
 //                .logLevel(Logger.Level.FULL)
                 .requestInterceptors(requestInterceptors)
+//                .options(feignOptions)
+                .retryer(retryer)
                 .target(NopCommerceClient.class, storeUrl);
 
         return nopCommerceClient;
